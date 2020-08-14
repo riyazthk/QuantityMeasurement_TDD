bbe5eeff7174160407cd6891349c408487d35d88
riyazthk <er.riyaz2507@gmail.com>
2020-08-14 19:46:31 +0530

    [Riyaz]ADDED:TO write a code to check reference of two value should not be equal

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,14 +1,27 @@
- import org.junit.Assert;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
- import quantitymeasurement.Feet;
+import quantitymeasurement.Feet;
 
 
- public class QuantityMeasurementTest {
-     @Test
-     public void givenZeroFtAndZeroFt_WhenCompared_ShouldReturnEquals() {
-         Feet feet1 = new Feet(0);
-         Feet feet2 = new Feet(0);
-         Assert.assertTrue(feet1.compare(feet2));
-     }
- }
+public class QuantityMeasurementTest {
+    @Test
+    public void givenZeroFtAndZeroFt_WhenCompared_ShouldBeEqual() {
+        Feet feet1 = new Feet(0);
+        Feet feet2 = new Feet(0);
+        Assert.assertTrue(feet1.compare(feet2));
+    }
+
+    @Test
+    public void givenZeroFtAndNull_WhenCompared_ShouldNotBeEqual() {
+        Feet feet1 = new Feet(0);
+        Assert.assertNotEquals(feet1, null);
+    }
+
+    @Test
+    public void givenZeroFtRefAndZeroFtRef_WhenCompared_ShouldNotBeEqual() {
+        Feet feet1 = new Feet(0);
+        Feet feet2 = new Feet(0);
+        Assert.assertNotEquals(feet1,feet2);
+    }
+}
