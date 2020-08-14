b0d5b0945152143fd496e000185846f864689f5f
riyazthk <er.riyaz2507@gmail.com>
2020-08-14 20:39:30 +0530

    [Riyaz]ADDED:To write a code to check two feet values equal or not

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -7,29 +7,37 @@ import quantitymeasurement.type;
 public class QuantityMeasurementTest {
     @Test
     public void givenZeroFtAndZeroFt_WhenCompared_ShouldBeEqual() {
-        Feet feet1 = new Feet(0,type.feet);
-        Feet feet2 = new Feet(0,type.feet);
+        Feet feet1 = new Feet(0, type.feet);
+        Feet feet2 = new Feet(0, type.feet);
         Assert.assertTrue(feet1.compare(feet2));
     }
 
     @Test
     public void givenZeroFtAndNull_WhenCompared_ShouldNotBeEqual() {
-        Feet feet1 = new Feet(0,type.feet);
+        Feet feet1 = new Feet(0, type.feet);
         Assert.assertNotEquals(feet1, null);
     }
 
     @Test
     public void givenZeroFtRefAndZeroFtRef_WhenCompared_ShouldBeEqual() {
-        Feet feet1 = new Feet(0,type.feet);
+        Feet feet1 = new Feet(0, type.feet);
         Feet feet2 = feet1;
-        Assert.assertEquals(feet1,feet2);
+        Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void givenTypeAndType_WhenCompared_ShouldBeEqual() {
-        Feet feet1 = new Feet(0,type.feet);
-        Feet feet2 = new Feet(0,type.feet);
-        Assert.assertEquals(feet1,feet2);
+        Feet feet1 = new Feet(0, type.feet);
+        Feet feet2 = new Feet(0, type.feet);
+        Assert.assertEquals(feet1, feet2);
+
+    }
+
+    @Test
+    public void givenFirstFtValAndSecondFtVal_WhenCompared_ShouldNotBeEqual() {
+        Feet feet1 = new Feet(0, type.feet);
+        Feet feet2 = new Feet(1, type.feet);
+        Assert.assertNotEquals(feet1, feet2);
 
     }
 }
