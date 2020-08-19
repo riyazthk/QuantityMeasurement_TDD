a084f505e930e5d42dd71373cff1b6cbabfebf89
riyazthk <er.riyaz2507@gmail.com>
2020-08-19 13:55:16 +0530

    [Riyaz]ADDED:To refactor a code to get a base unit

--- src/main/java/quantitymeasurement/QuantityMeasurementException.java
+++ src/main/java/quantitymeasurement/QuantityMeasurementException.java
@@ -3,13 +3,12 @@ package quantitymeasurement;
 public class QuantityMeasurementException extends Exception {
     public ExceptionType type;
 
-
     public enum ExceptionType {
         ADDITION_NOT_ALLOWED;
     }
 
-    public QuantityMeasurementException(String message, ExceptionType type){
+    public QuantityMeasurementException(String message, ExceptionType type) {
         super(message);
-        this.type=type;
+        this.type = type;
     }
 }
--- src/main/java/quantitymeasurement/Unit.java
+++ src/main/java/quantitymeasurement/Unit.java
@@ -1,20 +1,31 @@
 package quantitymeasurement;
 
 public enum Unit {
-    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4),
-    GALLON(3.78), MILLILITRES(0.001), LITRE(1.0),
-    KILOGRAM(1.0), TONNE(1000), GRAM(0.001),
-    FAHRENHEIT(1.0), CELSIUS(2.12);
+    FEET(12.0,"inch"), INCH(1.0,"inch"), YARD(36.0,"inch"), CENTIMETER(0.4,"inch"),
+    GALLON(3.78,"litre"), MILLILITRES(0.001,"litre"), LITRE(1.0,"litre"),
+    KILOGRAM(1.0,"kiloGram"), TONNE(1000,"kiloGram"), GRAM(0.001,"kiloGram"),
+    FAHRENHEIT(1.0,"celsius"), CELSIUS(2.12,"celsius");
 
 
     private final double baseUnitConversion;
+    private final String unit;
 
-    Unit(double baseUnitConversion) {
+    Unit(double baseUnitConversion,String unit) {
 
         this.baseUnitConversion = baseUnitConversion;
+        this.unit=unit;
     }
 
     public double convertToBaseUnit(double value) {
+
         return value * baseUnitConversion;
+        }
+
+    public Quantity convertToUnit(double value) {
+        String val= unit;
+        double result = value * baseUnitConversion;
+        return  new Quantity(val,result);
     }
+
+
 }
